8122aaf3c4f9dc40a0d0a80f4004edea6333a37a
anikmondol <anikmondol558363@gmil.com>
2024-03-22 15:49:23 +0600

    this video about math, abs, round, ceil, floor, and random number

---
+++ math.js
@@ -0,0 +1,37 @@
+// const Math = {
+//     min: function min(a, b, c) {
+//         return b;
+//     }
+// }
+
+// console.log(Math.min);
+// console.log(Math.max);
+
+
+const min = Math.min(10, 22, 47, 88, 92);
+// console.log(min);
+
+
+
+const max = Math.max(10, 22, 47, 88, 92);
+// console.log(max);
+
+
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-88 + 10));
+// console.log(Math.round(2.12));
+// console.log(Math.round(2.55));
+// console.log(Math.floor(2.999));
+// console.log(Math.ceil(3.11));
+
+
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+
+const rend =  Math.round(Math.random() * 10);
+console.log(rend);
+
