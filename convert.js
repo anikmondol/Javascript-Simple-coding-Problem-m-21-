27f22dcdd0a6f09ea54b60a4f0cd88001003b57f
anikmondol <anikmondol558363@gmil.com>
2024-03-22 13:56:19 +0600

    this video about convert many type

--- convert.js
+++ convert.js
@@ -1,10 +1,55 @@
-//  12 inch 1 feet
+// // 12 inch 1 feet
+
+// function inchToFeet(inch){
+//     const feet = inch / 12;
+//     return feet;
+// }
+
+// const anik = inchToFeet(69);
+// // console.log(anik);
+
+// // -------------------------------------
+
+// function feetToInch(feet){
+//     const inch = feet * 12;
+//     return inch;
+// }
+
+// const a = feetToInch(5.5);
+// // console.log(a);
+
+
+// // -------------------------------------------
+
 
 function inchToFeet(inch){
-    const feet = inch / 12;
-    return feet;
+    const feet = parseInt(inch / 12);
+    const inchRemaining = inch % 12;
+    return `${feet} ft ${inchRemaining} inch.`
 }
 
 
-const anik = inchToFeet(66);
+const anik = inchToFeet(66.50);
 console.log(anik);
+const ritu = inchToFeet(65);
+console.log(ritu);
+
+// ----------------------------------
+
+
+function mileToKilometer(mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+}
+
+const kilo = mileToKilometer(2);
+console.log(kilo);
+
+// ---------------------
+
+function KilometerToMile(kilo){
+    const mile = kilo * 0.621371;
+    return mile;
+}
+
+console.log(KilometerToMile(2));
